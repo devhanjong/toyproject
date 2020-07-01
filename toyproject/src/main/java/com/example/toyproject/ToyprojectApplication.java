5bf6b44c464c72e491631d29e510fab876473b1c
brave-son <solochangho@gmail.com>
2020-07-01 18:34:57 +0900

    111

--- toyproject/src/main/java/com/example/toyproject/ToyprojectApplication.java
+++ toyproject/src/main/java/com/example/toyproject/ToyprojectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ToyprojectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ToyprojectApplication.class, args);
+		SpringApplication.run(ToyprojectApplication.class, args); // 가나?
 	}
 
 }
