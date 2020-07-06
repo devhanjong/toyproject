00175da90f32b6b85361968c1a401677a709cb79
brave-son <solochangho@gmail.com>
2020-07-06 17:52:29 +0900

    12

--- toyproject/src/main/java/com/example/toyproject/controller/MemberController.java
+++ toyproject/src/main/java/com/example/toyproject/controller/MemberController.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.toyproject.model.Member;
 import com.example.toyproject.repository.MemberRepository;
