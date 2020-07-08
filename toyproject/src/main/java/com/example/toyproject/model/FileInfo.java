d52f3e002d40d8dc1598d30eff55a2582efee85a
kimkyunghwan-jinjinghuan <un5556@naver.com>
2020-07-08 14:24:38 +0900

    14:25

---
+++ toyproject/src/main/java/com/example/toyproject/model/FileInfo.java
@@ -0,0 +1,9 @@
+package com.example.toyproject.model;
+
+import org.springframework.web.multipart.MultipartFile;
+import lombok.Data;
+
+@Data
+public class FileInfo {
+	private MultipartFile file;
+}
