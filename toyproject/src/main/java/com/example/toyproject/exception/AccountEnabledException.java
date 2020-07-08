cb37633934135de173381fb469d893d66493080a
admin <admin@DESKTOP-NVKUSFJ>
2020-07-08 10:07:29 +0900

    7/8 10:07

--- toyproject/src/main/java/com/example/toyproject/exception/AccountEnabledException.java
+++ toyproject/src/main/java/com/example/toyproject/exception/AccountEnabledException.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.toyproject.exception;
 
 import org.springframework.security.core.AuthenticationException;
@@ -11,17 +10,3 @@ public class AccountEnabledException extends AuthenticationException {
 	}
 	
 }
-=======
-package com.example.toyproject.exception;
-
-import org.springframework.security.core.AuthenticationException;
-
-// 비활성 계정
-public class AccountEnabledException extends AuthenticationException {
-	
-	public AccountEnabledException(String msg) {
-		super(msg);
-	}
-	
-}
->>>>>>> branch 'master' of https://github.com/devhanjong/toyproject.git
